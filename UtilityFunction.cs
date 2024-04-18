287a76a764944e3637993b58e6b00fda39237535
Brijender Ahuja <er.brijenderahuja@gmail.com>
2024-04-19 00:07:40 +0530

    FirstCommit

---
+++ UtilityFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubCopilot
+{
+    public class UtilityFunction
+    {
+
+        // convert string to byte array
+        public static byte[] StringToByteArray(string str)
+        {
+            return Encoding.ASCII.GetBytes(str);
+        }
+        // write to find if array has duplicate values
+        public static bool HasDuplicateValues(int[] arr)
+        {
+            HashSet<int> hashSet = new HashSet<int>();
+            foreach (int i in arr)
+            {
+                if (!hashSet.Add(i))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        /// 
+
+    }
+
+}
